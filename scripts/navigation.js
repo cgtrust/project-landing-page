ce0b972f315eff153426fbb20400e4b7b4b1f5c0
Catharina Trust <144069315+cgtrust@users.noreply.github.com>
2024-01-23 19:54:12 -0800

    fixed javascript and styled mobile menu toggle

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,12 @@
-// Need to learn more JavaScript to enable button interaction
-( function () {
-    const siteNavigation = document.getElementById('nav')
-}
+// Navigation toggle
+const button = document.getElementById(
+    'menu-toggle'
+);
+const links = document.getElementById(
+    'header-menu'
+);
 
-)
+button.addEventListener( 'click', function() {
+    links.classList.toggle( 'toggled' );
+} );
+
